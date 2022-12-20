35098394b1552de73c3f667495d80c835985e264
Hshshahshsh <110442121+Hshshahshsh@users.noreply.github.com>
2022-12-20 14:53:00 +0800

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,3 @@
+_G.Key = "KChsjAsbskH"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script-v4-1.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/Main.lua"))()
